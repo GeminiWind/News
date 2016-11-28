1ab85719bc8552e857a4fe9ff89d84f8262e3c1e
Kominam <kominam.2511@gmail.com>
2016-11-28 21:44:32 +0700

    Update catagory pages :construction:

---
+++ Client/resources/views/frontend/pages/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>The News</title>
+  <link href="{{url('frontend/css/bootstrap.css')}}" rel='stylesheet' type='text/css' />
+  <!-- Custom Theme files -->
+  <link href="{{url('frontend/css/style.css')}}" rel="stylesheet" type="text/css" media="all" />
+  <!-- Custom Theme files -->
+  <script src="{{url('frontend/js/jquery.min.js')}}"></script>
+  <script type="text/javascript" src="{{url('frontend/js/jquery.leanModal.min.js')}}"></script>
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" />
+  <!-- Custom Theme files -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script type="application/x-javascript">
+    addEventListener("load", function() {
+      setTimeout(hideURLbar, 0); }, false);
+      function hideURLbar(){ window.scrollTo(0,1); }
+  </script>
+  <!--webfont-->
+</head>
+<body>
+  @include('frontend._partial.top')
+  @yield('content')
+  @include('frontend._partial.sidebar')
+  @include('frontend._partial.bottom')
+  @yield('extra-script')
+</body>
+</html>
